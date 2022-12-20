886f72f492185e554e0719daa26ff0c7c8e25660
YanSerov112 <100013018+YanSerov112@users.noreply.github.com>
2022-12-20 16:57:56 +0300

    v.1.1
    Im remake my last program and it can input in password a special symbols.

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,6 +1,7 @@
 import random
 
 PWNAns = []
+PWCS = random.randint(0,3)
 
 print("Генератор паролей v. 1.1")
 PWNTall = input("Введите нужное количество символов которое нужно в вашем пароле (цифра):")
@@ -9,8 +10,17 @@ for PWN in range(int(PWNTall)):
         PWN = random.randint(0,9)
         PWNAns.append(PWN)
 
-PWNANS = ''.join(map(str, PWNAns))
+if PWCS == 0:
+    PWCS = "-"
+elif PWCS == 1:
+    PWCS = "_"
+elif PWCS == 2:
+    PWCS = "!"
+elif PWCS == 3:
+    PWCS = "@"
 
+PWNAns.insert(random.randint(1, int(PWNTall) - 2), PWCS)
+PWNANS = ''.join(map(str, PWNAns))
 
 print("Ваш пароль: " + PWNANS)
 input()
