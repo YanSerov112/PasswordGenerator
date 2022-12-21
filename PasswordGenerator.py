9312516ac54f85f7d59a4e6902279aea0d9a3496
YanSerov112 <100013018+YanSerov112@users.noreply.github.com>
2022-12-21 21:07:30 +0300

    v.2.0
    Ok, Im maked a big update. Im added UI and removed all unnecessary questions to the user. I happy. Finally, I can send it to my friends! I going to drink a Cola. *fell on the bed*

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,57 +1,73 @@
 import random
+from tkinter import *
 
-PWNAns = []
-PWCS = random.randint(0,3)
+def rGnrt():
+    PWNAns = []
+    PWCS = random.randint(0,3)
+    PWNTall = random.randint(11,20)
+    PWLTall = random.randint(2,7)
+    
+    for PWN in range(int(PWNTall)):
+        PWN = random.randint(0,9)
+        PWNAns.append(PWN)
+    
+    PWL = random.randint(0,9)
+    if PWCS == 0:
+        PWCS = "-"
+    elif PWCS == 1:
+        PWCS = "_"
+    elif PWCS == 2:
+        PWCS = "!"
+    elif PWCS == 3:
+        PWCS = "@"
+    
+    for PWN in range(int(PWLTall)):
+        PWN = random.randint(0,9)
+        if PWL == 0:
+            PWL = "F"
+        elif PWL == 1:
+            PWL = "I"
+        elif PWL == 2:
+            PWL = "p"
+        elif PWL == 3:
+            PWL = "J"
+        elif PWL == 4:
+            PWL = "Q"
+        elif PWL == 5:
+            PWL = "u"
+        elif PWL == 6:
+            PWL = "W"
+        elif PWL == 7:
+            PWL = "x"
+        elif PWL == 8:
+            PWL = "Y"
+        elif PWL == 9:
+            PWL = "z"
+        PWNAns.insert(random.randint(0, int(PWNTall)), PWL)
+
+    PWNAns.insert(random.randint(1, int(PWNTall) - 2), PWCS)
+    PWNANS = ''.join(map(str, PWNAns))
+    print(PWNANS)
+    password["text"] = PWNANS
 
 
-print("Генератор паролей v. 1.2")
-PWNTall = input("Введите нужное количество символов которое нужно в вашем пароле (цифра):")
-PWLTall = input("Введите нужное количество букв которое нужно в вашем пароле (цифра):")
+root = Tk()
+root['bg'] = 'white'
+root.title = "Password Generator"
+root.geometry("250x200")
+root.resizable(width=False, height=False)
 
-for PWN in range(int(PWNTall)):
-    PWN = random.randint(0,9)
-    PWNAns.append(PWN)
+canvas = Canvas(root, height=300, width=250)
+canvas.pack()
 
-PWL = random.randint(0,9)
-if PWCS == 0:
-    PWCS = "-"
-elif PWCS == 1:
-    PWCS = "_"
-elif PWCS == 2:
-    PWCS = "!"
-elif PWCS == 3:
-    PWCS = "@"
+frame = Frame(root, bg='yellow')
+frame.place(relx=0.15, rely =0.15, relwidth=0.7, relheight=0.7)
 
-for PWN in range(int(PWLTall)):
-    PWN = random.randint(0,9)
-    if PWL == 0:
-        PWL = "F"
-    elif PWL == 1:
-        PWL = "I"
-    elif PWL == 2:
-        PWL = "p"
-    elif PWL == 3:
-        PWL = "J"
-    elif PWL == 4:
-        PWL = "Q"
-    elif PWL == 5:
-        PWL = "u"
-    elif PWL == 6:
-        PWL = "W"
-    elif PWL == 7:
-        PWL = "x"
-    elif PWL == 8:
-        PWL = "Y"
-    elif PWL == 9:
-        PWL = "z"
-    PWNAns.insert(random.randint(0, int(PWNTall)), PWL)
+title = Label(frame, text='Password generator', bg='yellow')
+title.pack()
+btn = Button(frame, text='Regenerate', bg='orange', command=rGnrt)
+btn.pack(side=BOTTOM)
+password = Label(frame, text='Password Here', bg='orange')
+password.pack()
 
-
-
-
-
-PWNAns.insert(random.randint(1, int(PWNTall) - 2), PWCS)
-PWNANS = ''.join(map(str, PWNAns))
-
-print("Ваш пароль: " + PWNANS)
-input()
+root.mainloop()
